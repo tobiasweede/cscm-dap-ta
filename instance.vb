16a81819c1460618c34840dbbeb46085aff2b489
Tobias Weiss <tobias@tobias-weiss.org>
2020-06-06 21:43:41 +0200

    autocommit

---
+++ instance.vb
@@ -0,0 +1,78 @@
+﻿Public Class instance
+    Public InbDes As Integer 'number inbound
+    Public OutDes As Integer 'number outbound
+
+    Public alphaA As Double 'start of the interval of alpha
+    Public alphaB As Double 'end of the interval of alpha
+
+    Public NumberOfSegments As Integer
+    Public DsIN As Integer 'number of inboutDes per Segment
+    Public DsOut As Integer 'number of inboutDes per Segment
+    Private OutPerIn() As Integer 'number of outbound destinations served by each inbound destination 
+    Private rnd As Random
+
+    Public bio(,) As Integer 'number of parcels to be shipped from inbound destination i to outbound destination o
+
+    Sub New(ByVal inbDes As Integer, ByVal outDes As Integer, ByVal alphaa As Double, ByVal alphab As Double)
+        Me.rnd = New Random
+        Me.InbDes = inbDes - 1
+        Me.OutDes = outDes - 1
+        Me.alphaA = alphaa
+        Me.alphaB = alphab
+        ReDim Me.OutPerIn(Me.InbDes)
+        ReDim Me.bio(Me.InbDes, Me.OutDes)
+
+        NumberOfSegments = CInt((2 / 3) * Math.Min(inbDes + 1, outDes + 1))
+
+        DsIN = Math.Round(inbDes / NumberOfSegments)
+        DsOut = Math.Round(outDes / NumberOfSegments)
+
+        OutPerIn = getOutPerIn(OutPerIn)
+        getBio()
+    End Sub
+
+    Function getOutPerIn(array() As Integer) 'support function for bio
+        Dim zValue(UBound(array)) As Integer
+
+        For i = 0 To UBound(zValue)
+            zValue(i) = CInt(getRND() * OutDes)
+        Next
+
+        Return zValue
+    End Function
+
+    Sub getBio() ' draw a number of parcels to be shipped from inbound destination i to outbound destination o, for each i and o
+        For i = 0 To InbDes
+            Dim RndSeq() As Integer
+            RndSeq = getRndOutSeq()
+            For j = 0 To OutPerIn(i) - 1
+                Dim RndAlpha As Double = CInt(getRND() * OutDes)
+                Dim Muy As Double = CInt(5000 / RndAlpha)
+                Dim sigma As Double = CInt(0.2 * 5000 / RndAlpha)
+                bio(i, RndSeq(j)) = rnd.Next(Muy - sigma, Muy + sigma)
+            Next
+        Next
+    End Sub
+
+    Function getRND() 'draw a random double in [alphaA,alphaB]
+        Dim zRnd As Double
+        Do
+            zRnd = rnd.NextDouble
+        Loop Until zRnd >= alphaA And zRnd <= alphaB
+        Return zRnd
+    End Function
+
+    Function getRndOutSeq() 'draw random sequence of outbound destinations
+        Dim outSeq(OutDes) As Integer
+        Dim zList As New List(Of Integer)
+        For i = 0 To OutDes
+            zList.Add(i)
+        Next
+        For i = 0 To OutDes
+            Dim zRnd As Integer = rnd.Next(0, zList.Count)
+            outSeq(i) = zList(zRnd)
+            zList.RemoveAt(zRnd)
+        Next
+        Return outSeq
+    End Function
+End Class
---
+++ main.vb
@@ -0,0 +1,100 @@
+﻿Module main
+
+    Sub Main()
+        Dim inDes As Integer = 3 '{3,5,7,25,50,75}
+        Dim outDes As Integer = 7 '{3,5,7,25,50,75}
+        Dim alphaa As Double = 0.3 '[0.1,0.5][0.6,0.9]
+        Dim alphab As Double = 0.5 '[0.1,0.5][0.6,0.9]
+
+        Dim instance As New instance(inDes, outDes, alphaa, alphab)
+
+        thresholdAccepting(instance)
+        Console.WriteLine("Press Enter to continue...")
+        Console.ReadLine()
+    End Sub
+
+    Function evaluateObjective(ByRef instance As instance, ByRef inboundDestination() As Integer, ByRef outboundDestionation() As Integer) As Integer
+        Dim objective As Integer = 0
+        For i = 0 To instance.InbDes - 1 ' for all inbound destionations
+            For j = 0 To instance.OutDes - 1 ' for all outbound destinations
+                objective += instance.bio(i, j) * (1 + Math.Abs(inboundDestination(i) - outboundDestionation(j)))
+            Next
+        Next
+        Return objective
+    End Function
+
+    Sub thresholdAccepting(ByRef instance As instance)
+        ' init
+        Dim temperature As Integer = 100 ' TA temperature
+        Dim stopTemperature As Integer = 2 ' stop temperature
+        Dim cooldownFactor As Double = 0.8 ' cooldown rate
+        Dim objective As Integer = 0 ' objective value
+        Dim inboundDestination(instance.InbDes - 1) As Integer ' array for inbound destinations
+        Dim outboundDestination(instance.OutDes - 1) As Integer ' array for outbound destinations
+        Dim bestOutboundDestination(instance.OutDes - 1) As Integer ' array for best outbound destinations
+
+        ' start solution
+        ' assign inbound destinations (fill up each sector sequentially)
+        Dim inboundAssigned As Integer = 0
+        For i = 0 To instance.InbDes - 1 ' for all inbound destionations
+            inboundDestination(i) = 1 + (inboundAssigned \ instance.DsIN) ' DsIn is maximum amout of inbound stations
+            inboundAssigned += 1
+        Next
+        ' assign outbound destinations (fill up each sector sequentially)
+        Dim outboundAssigned As Integer = 0
+        For j = 0 To instance.OutDes - 1 ' for all outbound destinations
+            outboundDestination(j) = 1 + (outboundAssigned \ instance.DsOut)
+            outboundAssigned += 1
+        Next
+        objective = evaluateObjective(instance, inboundDestination, outboundDestination) ' initial (best) objective
+        Console.WriteLine("initial objective: " & objective)
+        Console.WriteLine("initial output assignment: ")
+        For j = 0 To instance.OutDes - 1 ' for all outbound destinations
+            Console.Write(outboundDestination(j) & " ")
+        Next
+        Console.Write(Environment.NewLine)
+
+        ' TA main loop
+        Do
+            ' create neighbor solution
+            Dim newOutboundDestination(instance.OutDes - 1) As Integer ' array for new outbound destinations
+            outboundDestination.CopyTo(newOutboundDestination, 0)
+            ' get two random destinations to swap
+            Dim rnd = New Random
+            Dim dst1Idx As Integer = rnd.Next(instance.OutDes - 1) ' random index for first destination to swap
+            Dim dst2Idx As Integer ' random index for second destination to swap
+            Do ' dst1 and dst2 must be different!
+                dst2Idx = rnd.Next(instance.OutDes - 1)
+            Loop While dst1Idx = dst2Idx
+            ' swap two elements within newOutboundDestination
+            newOutboundDestination(dst1Idx) = outboundDestination(dst2Idx)
+            newOutboundDestination(dst2Idx) = outboundDestination(dst1Idx)
+            Dim newObjective As Integer = evaluateObjective(instance, inboundDestination, newOutboundDestination) ' initial (best) objective
+            ' check if maximum output destinations per segment is violated
+            Dim outputsPerSegment() As Integer = Enumerable.Repeat(0, instance.NumberOfSegments).ToArray  ' array to count outputs per segment initialized with zeros
+            Dim validSolution As Boolean = True
+            For j = 0 To instance.OutDes - 1 ' count outbound destinations for all segments
+                outputsPerSegment(newOutboundDestination(j) - 1) += 1
+                If outputsPerSegment(newOutboundDestination(j) - 1) > instance.DsOut Then
+                    validSolution = False
+                    Exit For
+                End If
+            Next
+            ' save new (valid) solution if objective is better
+            If validSolution And newObjective <= objective Then
+                objective = newObjective
+                newOutboundDestination.CopyTo(outboundDestination, 0)
+            End If
+            temperature *= cooldownFactor ' cooldown
+            Debug.WriteLine("temperature: " & temperature)
+        Loop While stopTemperature < temperature
+
+        Console.WriteLine("best objective: " & objective)
+        Console.WriteLine("best output assignment: ")
+        For j = 0 To instance.OutDes - 1 ' for all outbound destinations
+            Console.Write(outboundDestination(j) & " ")
+        Next
+        Console.Write(Environment.NewLine)
+    End Sub
+
+End Module
